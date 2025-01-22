0ece73b8b2e6873fd385ee03a1031b098b56681e
ChNathan <66319564+ChNathan@users.noreply.github.com>
2025-01-22 16:44:45 -0500

    Update index.js

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Navbar from '@theme-original/Navbar';
 import ShopHeader from '../../components/ShopHeader';
 
+const StickyNavbar = (props) => {
+    return <Navbar {...props} className="sticky-navbar" />;
+};
+
+export default StickyNavbar;
+
 export default function NavbarWrapper(props) {
   return (
     <>
